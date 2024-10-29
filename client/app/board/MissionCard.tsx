56b41582b019924538a5f89813adda27867e245d
Rômulo Henri <romulohenri000@gmail.com>
2024-10-28 23:57:00 -0300

    improve (MissionCard.tsx): Component

--- client/app/board/MissionCard.tsx
+++ client/app/board/MissionCard.tsx
@@ -1,9 +1,20 @@
 "use client";
 
 import { useRouter } from 'next/navigation';
+import { motion } from 'framer-motion';
 import { getODSColor } from '@common/handleODS';
 import { getStatusText } from '@common/handleMissions';
 
+import {
+  CircleDot as PointIcon,
+} from 'lucide-react';
+
+import { Baloo_Chettan_2 } from 'next/font/google';
+
+const balooChettan = Baloo_Chettan_2({
+  weight: '600',
+  subsets: ['latin'],
+});
 interface MissionCardProps {
   id: number;
   ods: number;
@@ -13,6 +24,7 @@ interface MissionCardProps {
   rating: number;
   points: number;
   status: "done" | "doing" | "todo";
+  anim?: "right" | "center";
 }
 
 const MissionCard: React.FC<MissionCardProps> = ({ 
@@ -22,7 +34,8 @@ const MissionCard: React.FC<MissionCardProps> = ({
   summary,
   rating,
   points,
-  status = "todo"
+  status = "todo",
+  anim = "center" // Valor padrão
 }) => {
   const router = useRouter();
   const statusInfo = getStatusText(status);
@@ -31,10 +44,33 @@ const MissionCard: React.FC<MissionCardProps> = ({
     router.push(`/missions/${id}`);
   };
 
+  const animationConfig =
+    anim === "right"
+      ? {
+          initial: { opacity: 0, x: -11 },
+          animate: { opacity: 1, x: 0 },
+          transition: { duration: 0.2, ease: "easeOut" },
+          whileHover: { x: 5 },
+          whileTap: { x: 15 },
+        }
+      : {
+          initial: { opacity: .5, scale: 0.98 },
+          animate: { opacity: 1, scale: 1 },
+          transition: { duration: 0.3, ease: "easeOut" },
+          whileHover: { scale: 1 },
+          whileTap: { scale: 0.95 },
+        };
+
   return (
-    <div 
+    <motion.div 
+      style={{
+        willChange: "transform",
+        backfaceVisibility: "hidden",
+        transformStyle: "preserve-3d"
+      }}
       onClick={handleClick}
-      className="max-w-md w-full p-2 bg-slate-200 rounded-lg shadow-md flex items-center space-x-4 min-h-[104px] cursor-pointer hover:bg-slate-300 transition-colors"
+      {...animationConfig}
+      className="max-w-md w-full p-2 bg-slate-200 rounded-lg shadow-md flex items-center space-x-4 min-h-[104px] cursor-pointer hover:bg-[#d2dde9] transition-colors"
     >
       <div className="w-16 h-full rounded-lg overflow-hidden flex justify-center flex-col items-center">
         <div
@@ -42,20 +78,24 @@ const MissionCard: React.FC<MissionCardProps> = ({
         >
           {ods}
         </div>
-        <div className="text-xs font-bold text-gray-700 mt-2">
-          {points} pts
+        <div className={`flex text-xs font-bold text-blue-700  mt-2 items-center ${balooChettan.className}`}>
+          {points}
+          <PointIcon
+            className="ml-1"
+            size={12}
+            strokeWidth={3}
+          />
         </div>
       </div>
 
       <div className="flex-1">
         <div className="flex justify-between items-center">
-          <h3 className="text-green-800 flex font-semibold text-md">
+          <h3 className="text-[#20626b] flex font-semibold text-md">
             {title}
           </h3>
         </div>
 
-        <p className="text-gray-500 text-xs line-clamp-2 
-        min-h-[32px]">
+        <p className="text-gray-500 text-xs line-clamp-2 min-h-[32px]">
           {summary}
         </p>
 
@@ -66,13 +106,24 @@ const MissionCard: React.FC<MissionCardProps> = ({
               <span className="text-sm font-semibold">{rating}</span>
             </div>
           </div>
-          <div className={`w-36 mt-0 font-bold text-sm ${statusInfo.color} border-2 border-[#338F9B50]  border-t-0 border-l-0 text-right rounded-ee`}  
-          >
-            {statusInfo.text}
+          <div className={`w-28 mt-0 text-xs uppercase ${statusInfo.color}`}>
+            <div
+              className={`${
+                status === 'done'
+                  ? 'bg-[#65ac88] text-gray-200' 
+                  : status === 'todo'
+                  ? 'border-2 border-gray-300 text-gray-500' 
+                  : status === 'doing'
+                  ? 'border-2 border-gray-300 text-gray-500' 
+                  : 'bg-[#2d8f5e90]'
+              } text-center rounded`}
+            >
+              {statusInfo.text}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
--- client/app/board/page.tsx
+++ client/app/board/page.tsx
@@ -5,6 +5,7 @@ export default function Board() {
   return (
     <main className="flex-grow flex justify-start items-center bg-slate-300 px-2 pt-2 pb-20 md:pb-0 gap-2 flex-col overflow-hidden">
       <div className="flex flex-col items-center gap-4 h-full max-h-[calc(100vh-130px)] w-full overflow-y-auto scrollbar-thin scrollbar-thumb-blue-600 scrollbar-track-gray-300 px-2 pt-2">
+
         {MISSIONS.map((mission, index) => (
           <MissionCard
             key={index}
@@ -15,7 +16,8 @@ export default function Board() {
             rating={mission.rating}
             points={mission.points}
             ods={mission.ods}
-            status={["done", "doing", "todo"].includes(mission.status) ? (mission.status as "done" | "doing" | "todo") : "todo"} 
+            anim="right"
+            status={["done", "doing", "todo"].includes(mission.status) ? (mission.status as "done" | "doing" | "todo") : "todo"}
           />
         ))}
       </div>
