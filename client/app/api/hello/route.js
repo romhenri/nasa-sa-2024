0470b23d5362a5d3772bf16c2ae2a21f7a62186f
Rômulo Henri <romulohenri000@gmail.com>
2024-10-31 23:30:18 -0300

    feat (app/api): Next API

---
+++ client/app/api/hello/route.js
@@ -0,0 +1,11 @@
+// "/hello"
+
+export async function GET(request) {
+    return new Response(JSON.stringify({ message: 'Hello, World!' }), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+  
