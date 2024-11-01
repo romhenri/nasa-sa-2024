ba0abf10c2492ba3f4bed92e246a425d704849e2
Rômulo Henri <romulohenri000@gmail.com>
2024-10-31 23:45:09 -0300

    feat (app/api): App About Endpoint

--- client/app/api/about/route.js
+++ client/app/api/about/route.js
@@ -1,9 +1,9 @@
-// "/team"
+// "/about"
 
-import TEAM from '@data/team.json';
+import APP from '@data/app.json';
 
 export async function GET(request) {
-  return new Response(JSON.stringify(TEAM), {
+  return new Response(JSON.stringify(APP), {
     status: 200,
     headers: {
       'Content-Type': 'application/json',
