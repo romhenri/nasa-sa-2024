e65f448504e8e783d013e7ff9b9502de7a621165
Rômulo Henri <romulohenri000@gmail.com>
2025-01-10 23:28:46 -0300

    feat (server): Class Endpoint Filter by Score

--- server/src/class/class.controller.ts
+++ server/src/class/class.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { ClassService } from './class.service';
 
 @Controller('class')
 export class ClassController {
   constructor(private readonly classService: ClassService) {}
 
+  /**
+   * Retorna todas as turmas sem filtros.
+   */
   @Get()
-  findAll(): string {
-    return this.classService.getClass();
+  findAll(): any {
+    return this.classService.getAllClasses();
+  }
+
+  /**
+   * Retorna uma turma específica e permite filtrar os alunos por `score`.
+   */
+  @Get(':id')
+  findOne(
+    @Param('id') id: string,
+    @Query('min_score') minScore?: string,
+    @Query('max_score') maxScore?: string
+  ): any {
+    return this.classService.getClassByIdWithFilter(
+      id,
+      minScore ? Number(minScore) : undefined,
+      maxScore ? Number(maxScore) : undefined
+    );
   }
 }
--- server/src/class/class.service.ts
+++ server/src/class/class.service.ts
@@ -3,8 +3,52 @@ import { readFileSync } from 'fs';
 
 @Injectable()
 export class ClassService {
-  getClass(): string {
-    const data = readFileSync('./data/class.json', 'utf8');
-    return JSON.parse(data)
+  private readonly filePath = './data/class.json';
+
+  /**
+   * Retorna todas as turmas sem filtros.
+   */
+  getAllClasses(): any {
+    return this.readData();
+  }
+
+  /**
+   * Retorna uma turma pelo ID e filtra os alunos pelo score, se necessário.
+   * @param id ID da turma
+   * @param minScore Valor mínimo de score (opcional)
+   * @param maxScore Valor máximo de score (opcional)
+   */
+  getClassByIdWithFilter(id: string, minScore?: number, maxScore?: number): any {
+    const classes = this.readData();
+    const classId = Number(id);
+
+    const foundClass = classes.find((c: any) => c.id === classId);
+
+    if (!foundClass) {
+      return { message: 'Not Found', };
+    }
+
+    if (minScore !== undefined || maxScore !== undefined) {
+      foundClass.students = foundClass.students.filter((student: any) => {
+        return (
+          (minScore === undefined || student.score >= minScore) &&
+          (maxScore === undefined || student.score <= maxScore)
+        );
+      });
+    }
+
+    return foundClass;
+  }
+
+  /**
+   * Lê o arquivo JSON e retornar os dados.
+   */
+  private readData(): any {
+    try {
+      const data = readFileSync(this.filePath, 'utf8');
+      return JSON.parse(data);
+    } catch (error) {
+      throw new Error('Erro ao ler o arquivo class.json');
+    }
   }
 }
