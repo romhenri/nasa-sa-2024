631fcc782c342574dcfe13fd7ebc1933f1da7812
Rômulo Henri <romulohenri000@gmail.com>
2025-01-15 22:00:51 -0300

    feat (server): Students Endpoint

--- server/src/class/class.controller.ts
+++ server/src/class/class.controller.ts
@@ -8,15 +8,13 @@ export class ClassController {
   constructor(private readonly classService: ClassService) {}
 
   /**
-   * Retorna todas as turmas, permitindo filtros opcionais por nome, professor e ano.
+   * Retorna todas as turmas, permitindo Filtros
    */
   @Get()
-  @ApiOperation({ summary: 'Listar Turmas com Filtros Opcionais' })
+  @ApiOperation({ summary: 'Listar Turmas com Filtros' })
   @ApiQuery({ name: 'name', required: false })
   @ApiQuery({ name: 'teacher', required: false })
   @ApiQuery({ name: 'year', required: false })
-  @ApiQuery({ name: 'minScore', required: false })
-  @ApiQuery({ name: 'maxScore', required: false })
   findAll(
     @Query('name') name?: string,
     @Query('teacher') teacher?: string,
@@ -36,5 +34,22 @@ export class ClassController {
   @ApiOperation({ summary: 'Buscar uma Turma pelo ID' })
   findOne(@Param('id') id: string): any {
     return this.classService.getClassById(id);
+  }
+
+  /**
+   * Retorna os alunos de uma turma com Filtros
+   */
+  @Get(':id/students')
+  @ApiOperation({ summary: 'Listar Alunos de uma Turma com Filtros' })
+  @ApiQuery({ name: 'min_score', required: false })
+  @ApiQuery({ name: 'max_score', required: false })
+  findStudents(@Param('id') id: string,
+    @Query('min_score') minScore?: string,
+    @Query('max_score') maxScore?: string
+  ): any {
+    return this.classService.getStudents(id, {
+      minScore: Number(minScore),
+      maxScore: Number(maxScore),
+    });
   }
 }
--- server/src/class/class.service.ts
+++ server/src/class/class.service.ts
@@ -3,14 +3,15 @@ import { readFileSync } from 'fs';
 
 @Injectable()
 export class ClassService {
-  private readonly filePath = './data/class.json';
+  private readonly classPath = './data/class.json';
+  private readonly studentsPath = './data/students.json';
 
   /**
    * Retorna todas as turmas, aplicando filtros opcionais se fornecidos.
    * @param filters Filtros opcionais para a listagem
    */
-  getClasses(filters?: { name?: string; teacher?: string; year?: number; minScore?: number; maxScore?: number }): any {
-    let classes = this.readData();
+  getClasses(filters?: { name?: string; teacher?: string; year?: number}): any {
+    let classes = this.readClassData();
 
     if (!filters) return classes;
 
@@ -19,14 +20,23 @@ export class ClassService {
       (!filters.name || c.name.toLowerCase().includes(filters.name.toLowerCase())) &&
       (!filters.teacher || c.teacher.toLowerCase().includes(filters.teacher.toLowerCase())) &&
       (!filters.year || c.year === filters.year)
-    )
-    .map((c: any) => ({
-      ...c,
-      students: c.students ? c.students.filter((student: any) =>
-        (!filters.minScore || student.score >= filters.minScore) &&
-        (!filters.maxScore || student.score <= filters.maxScore)
-      ) : []
-    }));
+    );
+  }
+
+  getStudents(id: string, filter ?: { minScore?: number; maxScore?: number }): any {
+    const classes = this.readStudentsData();
+    const classId = Number(id);
+
+    const students = classes.filter((student: any) => student.classId === classId);
+
+    if (!students) {
+      return { message: 'Alunos não encontrados' };
+    }
+
+    return students.filter((student: any) =>
+      (!filter.minScore || student.score >= filter.minScore) &&
+      (!filter.maxScore || student.score <= filter.maxScore)
+    );
   }
 
   /**
@@ -34,7 +44,7 @@ export class ClassService {
    * @param id ID da turma
    */
   getClassById(id: string): any {
-    const classes = this.readData();
+    const classes = this.readClassData();
     const classId = Number(id);
 
     const foundClass = classes.find((c: any) => c.id === classId);
@@ -45,13 +55,21 @@ export class ClassService {
 
     return foundClass;
   }
+  
+  private readClassData(): any {
+    return this.readData(this.classPath);
+  }
+
+  private readStudentsData(): any {
+    return this.readData(this.studentsPath);
+  }
 
   /**
    * Lê o arquivo JSON e retorna os dados.
    */
-  private readData(): any {
+  private readData(path: string): any {
     try {
-      const data = readFileSync(this.filePath, 'utf8');
+      const data = readFileSync(path, 'utf8');
       return JSON.parse(data);
     } catch (error) {
       throw new Error('Erro ao ler o arquivo class.json');
